427b9b8c621ca9ffb207a856c4cc908734bfef85
Tergel Enkhtur <tergel.e9@gmail.com>
2023-01-14 22:46:28 -0600

    72 and more

--- src/main/java/guru/springframework/sfdi/SfDiApplication.java
+++ src/main/java/guru/springframework/sfdi/SfDiApplication.java
@@ -4,12 +4,18 @@ import guru.springframework.sfdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.springframework.sfdi", "com.springframework.pets"})
 @SpringBootApplication
 public class SfDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
 
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
@@ -20,21 +26,15 @@ public class SfDiApplication {
 		System.out.println(myController.sayHello());
 
 		System.out.println("------ Property");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
 		System.out.println("------ Setter");
-
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("------ Constructor");
-
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
 		System.out.println(constructorInjectedController.getGreeting());
 	}
 
